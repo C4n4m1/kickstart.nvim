fca063ac6bff1f3f80f75842ff6718753f5c998a
C4n4m1 <hcanami@gmail.com>
2025-03-05 21:14:53 +0000

    basic plugins

---
+++ lua/custom/plugins/typr.lua
@@ -0,0 +1,29 @@
+return {
+  'nvzone/typr',
+  dependencies = 'nvzone/volt',
+  opts = {
+
+    mode = 'words', -- words, phrases
+    winlayout = 'responsive',
+    kblayout = 'azerty',
+    wpm_goal = 75,
+    numbers = false,
+    symbols = false,
+    random = false,
+    phrases = nil, -- can be a table of strings
+    insert_on_start = false,
+    stats_filepath = vim.fn.stdpath 'data' .. '/typrstats',
+    mappings = nil,
+    -- or function(buf) end
+    -- mappings = function(buf)
+    --  vim.keymap.set("n", "a, anything, { buffer = buf })
+    -- end,
+    on_attach = nil,
+    -- or function(buf) end
+    -- on_attach = function(buf)
+    --  vim.b[buf].minipairs_disable = true
+    -- end,
+  },
+
+  cmd = { 'Typr', 'TyprStats' },
+}
