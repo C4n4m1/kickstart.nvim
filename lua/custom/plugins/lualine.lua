d6918ffd35a543962e56093b2758746d15dc9a12
C4n4m1 <hcanami@gmail.com>
2026-03-02 20:14:49 +0000

    new status line

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,6 +2,66 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = { theme = 'mellow' },
+    config = function()
+      local colors = {
+        black = '#070709',
+        gray = '#252528',
+        white = '#C9C7CD',
+        darkgray = '#151518',
+        lightgray = '#505052',
+        inactivegray = '#333335',
+      }
+
+      local theme = {
+        normal = {
+          a = { bg = colors.gray, fg = colors.white, gui = 'bold' },
+          b = { bg = colors.inactivegray, fg = colors.white },
+          c = { bg = colors.darkgray, fg = colors.white },
+        },
+        insert = {
+          a = { bg = colors.inactivegray, fg = colors.white, gui = 'bold' },
+          b = { bg = colors.gray, fg = colors.white },
+          c = { bg = colors.darkgray, fg = colors.white },
+        },
+        visual = {
+          a = { bg = colors.lightgray, fg = colors.white, gui = 'bold' },
+          b = { bg = colors.inactivegray, fg = colors.white },
+          c = { bg = colors.darkgray, fg = colors.white },
+        },
+        replace = {
+          a = { bg = colors.lightgray, fg = colors.white, gui = 'bold' },
+          b = { bg = colors.inactivegray, fg = colors.white },
+          c = { bg = colors.darkgray, fg = colors.white },
+        },
+        command = {
+          a = { bg = colors.darkgray, fg = colors.white, gui = 'bold' },
+          b = { bg = colors.gray, fg = colors.white },
+          c = { bg = colors.darkgray, fg = colors.white },
+        },
+        inactive = {
+          a = { bg = colors.darkgray, fg = colors.white, gui = 'bold' },
+          b = { bg = colors.darkgray, fg = colors.white },
+          c = { bg = colors.darkgray, fg = colors.white },
+        },
+      }
+
+      require('lualine').setup {
+        options = {
+          theme = theme,
+          -- component_separators = { left = '', right = '' },
+          -- section_separators = { left = '', right = '' },
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = {},
+          lualine_x = { 'filename' },
+          lualine_y = { 'progress', 'location' },
+          lualine_z = { 'filetype' },
+        },
+      }
+    end,
   },
 }
